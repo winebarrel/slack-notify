23ddd239511df7f785dad63c06f3c95651d0d932
Genki Sugawara <sugawara@cookpad.com>
2016-05-26 10:02:28 +0900

    Fix fomula

--- homebrew/slack-notify.rb
+++ homebrew/slack-notify.rb
@@ -5,7 +5,7 @@ class SlackNotify < Formula
 
   homepage 'https://github.com/winebarrel/slack-notify'
   url "https://github.com/winebarrel/slack-notify/releases/download/v#{VERSION}/slack-notify-v#{VERSION}-darwin-amd64.gz"
-  sha256 '71a507dc4228c024ca132773c9b8abdac7056cd024dddf67d01cfdb81f8a6056'
+  sha256 '8a61240c891cde8d7cfc9a50044cfe8d55d63fdeb8c15d044d7f76e149d4bece'
   version VERSION
   head 'https://github.com/winebarrel/slack-notify.git', :branch => 'master'
 
